9efdb3f7463f68c9d77aaf4a3cb2bc17f11f7982
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-10-20 12:08:49 -0400

    now serves html file at root

--- goodnotevil.py
+++ goodnotevil.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/login")
 def login():
+    return "login"
 
-
-   return "Hello World!"
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run()
