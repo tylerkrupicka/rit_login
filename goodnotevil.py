45586e3d28ac7079300edc50caeb623914ec6cb2
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-10-21 16:25:23 -0400

    port 80

--- goodnotevil.py
+++ goodnotevil.py
@@ -38,4 +38,4 @@ def index():
 if __name__ == '__main__':
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=80)
